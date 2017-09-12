17e72ce25e3cedbb6cd5ac913e9b28055a7c1f46
cmoser96 <cm898946@gmail.com>
2017-09-12 02:09:21 -0400

    updating estop

--- src/tractor/estop.cpp
+++ src/tractor/estop.cpp
@@ -2,43 +2,16 @@
 
 /******************************************************************************
  * Estop class for OAK (Olin Autonomous Kore)
- * @author: Carl Moser
- * @email: carl.moser@students.olin.edu
+ * @file estop.cpp
+ * @author Carl Moser
+ * @email carl.moser@students.olin.edu
+ * @version     1.1
  *
  * This is meant to be a modular class for any robot within the lab
- * it automatically creates a subscriber and publisher for estop(s)
- * true is stop
+ * it automatically creates a publisher and subscriber for an estop
  *
- * TODO: add support for multiple estop pins
- * TODO: check the state of the pin(s) on change
- *
- * TO USE:
- *  1- in the config header #define ESTOP_PIN and ESTOP_DEBOUNCE_TIME
- *  2- include the header in the .ino file (#include "estop.h")
- *  3- declare a pointer to the class (Estop *e)
- *  4- within void setup do the following:
- *    a- call the setup function with the memory address of the nodehandle
- *       (e->setup(&nh))
- *    b- set the function you want to run on a stop (e->onStop(stop)) where
- *       stop is a function
- *    c- set the function you want to run after a stop (e->offStop(stop)) where
- *       restart is a function
- *  5- that is it...the class will take care of the rest
+ * @class Estop estop.h "estop.h"
  ******************************************************************************/
-
-/*
- * Function and variable declarations
- * Needed since the class is static
- */
-ros::Publisher *Estop::hardEStop;
-ros::Subscriber<std_msgs::Bool> *Estop::softEStop;
-std_msgs::Bool Estop::stopped;
-bool Estop::softStopped = false;
-long Estop::last_mill;
-void Estop::defaultOnStop(){};
-void Estop::defaultOffStop(){};
-void (*Estop::stopfunc)() = defaultOnStop;
-void (*Estop::startfunc)() = defaultOffStop;
 
 /*
  * Setup function for the class
@@ -46,15 +19,23 @@ void (*Estop::startfunc)() = defaultOffStop;
  * Initializes a publisher and subsciber
  * attaches the estop pin
  */
-void Estop::setup(ros::NodeHandle *nh){
+Estop::Estop(ros::NodeHandle *nh, const int pin, const unsigned int debounceTime):pin(pin),debounceTime(debounceTime){
   hardEStop = new ros::Publisher("/hardestop", &stopped);
-  softEStop = new ros::Subscriber<std_msgs::Bool>("/softestop", &softStopCB);
+  softEStop = new ros::Subscriber<std_msgs::Bool, Estop>("/softestop", &Estop::softStopCB, this);
   nh->advertise(*hardEStop);
   nh->subscribe(*softEStop);
   last_mill = millis();
-  pinMode(ESTOP_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ESTOP_PIN), onChange, CHANGE);
-  stopped.data = !digitalRead(ESTOP_PIN);
+  pinMode(pin, INPUT_PULLUP);
+  attachInterrupt2(digitalPinToInterrupt(pin), &Estop::globalStop, CHANGE, this);
+  stopped.data = !digitalRead(pin);
+}
+
+/*
+ * Global function that calls the object function
+ * @param[in] instance Instance of the class
+ */
+void Estop::globalStop(void *instance){
+  static_cast<Estop*>(instance)->onChange();
 }
 
 /*
@@ -64,15 +45,25 @@ void Estop::setup(ros::NodeHandle *nh){
  * If reseting from stop - calls the startfunc and publishes false
  */
 void Estop::onChange(){
-  if(millis()-last_mill >= ESTOP_DEBOUNCE_TIME*50){
-    stopped.data = !stopped.data;
-    hardEStop->publish(&stopped);
-    if(stopped.data){
+  if(millis()-last_mill >= debounceTime*50){
+    /*if(digitalRead(ESTOP_PIN)){
+      stopped.data = true;
+      hardEStop->publish(&stopped);
       (*Estop::stopfunc)();
     }
     else{
+      stopped.data = false;
+      hardEStop->publish(&stopped);
+      (*Estop::startfunc)();
+    }*/
+    stopped.data = !stopped.data;
+    hardEStop->publish(&stopped);
+    if(stopped.data){
+      (*stopfunc)();
+    }
+    else{
       if(!softStopped)
-        (*Estop::startfunc)();
+        (*startfunc)();
     }
     last_mill = millis();
   }
@@ -83,26 +74,20 @@ void Estop::onChange(){
  */
 void Estop::softStopCB(const std_msgs::Bool &message){
   if(message.data){
-    (*Estop::stopfunc)();
+    (*stopfunc)();
     softStopped = true;
   }
   else{
     softStopped = false;
     if(!stopped.data)
-      (*Estop::startfunc)();
+      (*startfunc)();
   }
 }
 
-/*
- * Set stop function pointer to function pointer that is passed in
- */
 void Estop::onStop(void (*func)()){
   stopfunc = func;
 }
 
-/*
- * Set restart function pointer to function pointer that is passed in
- */
 void Estop::offStop(void (*func)()){
   startfunc = func;
 }
--- src/tractor/estop.h
+++ src/tractor/estop.h
@@ -1,46 +1,31 @@
 #ifndef ESTOP_H
 #define ESTOP_H
 
-/******************************************************************************
- * Estop class for OAK (Olin Autonomous Kore)
- * @author: Carl Moser
- * @email: carl.moser@students.olin.edu
- ******************************************************************************/
-
 #include "ros.h"
 #include "std_msgs/Bool.h"
-#include "config.h"
 
-#ifndef ESTOP_PIN
-	#error THE ESTOP PIN NEEDS TO BE DEFINED
-#endif
-
-#ifndef ESTOP_DEBOUNCE_TIME
- #pragma THE ESTOP DEBOUNCE TIME WAS NOT DEFINED - DEFAULTING TO 1
- #define ESTOP_DEBOUNCE_TIME 1 // half-milliseconds
-#endif
+static void dummyFunc() {return;}
+extern void attachInterrupt2(uint8_t pin, void (*function)(void*), int mode, void* clas);
 
 class Estop{
 public:
-	/*
-	 * The only functions users should be concerned with
-	 */
-	static void setup(ros::NodeHandle *nh);
-	static void onStop(void (*func)());
-	static void offStop(void (*func)());
+	explicit Estop(ros::NodeHandle *nh, const int pin, const unsigned int debounceTime);
+	static void globalStop(void* instance);
+	void onStop(void (*func)());
+	void offStop(void (*func)());
 
 private:
-  static ros::Publisher *hardEStop;
-	static ros::Subscriber<std_msgs::Bool> *softEStop;
-  static std_msgs::Bool stopped;
-	static bool softStopped;
-	static long last_mill;
-	static void (*stopfunc)();
-	static void (*startfunc)();
-  static void onChange();
-	static void softStopCB(const std_msgs::Bool &message);
-	static void defaultOnStop();
-	static void defaultOffStop();
+  ros::Publisher *hardEStop;
+	ros::Subscriber<std_msgs::Bool, Estop> *softEStop;
+  std_msgs::Bool stopped;
+	bool softStopped = false;
+	const unsigned int debounceTime;
+	const int pin;
+	long last_mill;
+	void (*stopfunc)() = dummyFunc();
+	void (*startfunc)() = dummyFunc();
+  void onChange();
+	void softStopCB(const std_msgs::Bool &message);
 };
 
 #endif //ESTOP_H
