7ab46798b735b4228f0859812b9ffb5e488a14f1
nestill <nathan.estill@students.olin.edu>
2017-10-29 16:41:00 -0400

    updating

--- scripts/LidarCodeBasic.py
+++ scripts/LidarCodeBasic.py
@@ -10,15 +10,14 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 
 def callback(data):
-    
-    totalDist= []
-    i = 0
-    while i < len(data.ranges):
-        totalDist.append(data.ranges[i])
-        if totalDist[i] > 1000000:
+    totalDist= [] #Make a new array, this stuff is currently just for debugging, and unneccesary
+    i = 0 # Using a manual for loop because i dont know python
+    while i < len(data.ranges): # for loop stuff
+        totalDist.append(data.ranges[i]) # assign each range to the array to make it canfigurable
+        if totalDist[i] > 1000000: # converting all 'inf' to an actual number
             totalDist[i] = 100000 
-        i += 1
-    otherCode(data)
+        i += 1 # parsing
+    otherCode(data) #calling below code
     #print(totalDist[600])
 def listener():
 
@@ -27,22 +26,20 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('scan',LaserScan,callback)
-    rospy.spin()
+    rospy.init_node('listener', anonymous=True) # idk what this does tbh
+    rospy.Subscriber('scan',LaserScan,callback) # Subscribes to the laser thing, calls the callback function
+    rospy.spin() # loops it
 def otherCode(data):
 # Code is supposed to detect if there is an obstacle, and if so, stop the tractor
-# lines with *** coordinated to the bravobot - 10/15/17
-# Things do to before completion:
-# Actually import data -- Done
-# Find out how to stop the tractor -- In Progress
-# Test various values to make sure it works
-    i = 0
-    ang_res = 0.25
+# Rougly complete, may not work in parcel B
+    pub0 = rospy.Publisher('/estop', Bool,  queue_size=10) # init your publishers early
+    pub1 = rospy.Publisher('/scan_verticals', Float64,  queue_size=10)
+    pub2 = rospy.Publisher('/scan_horizontals', Float64,  queue_size=10) 
     # puts the distance away into a separate variable # Converts step to degrees
-    totalDist = []
-    verticalDistance = []
-    horizontalDistance = []
+    totalDist = [] #Setting arrays
+    verticalDistance = [] # Distance from front of tractor
+    horizontalDistance = [] # Distance from center of tractor
+    i = 0
     while i < len(data.ranges):
         totalDist.append(data.ranges[i])
         if totalDist[i] > 1000000:
@@ -50,53 +47,40 @@ def otherCode(data):
         verticalDistance.append(abs(math.cos(math.radians((i - 380.0) * (190.0 / 760.0))) * totalDist[i])) # Computes the distance from the object
         horizontalDistance.append(math.sin(math.radians((i - 380.0) * (190.0 / 760.0))) * totalDist[i]) # Computes the distance parallel to tractor
         i += 1
-    #horizontalDistance = [abs(sin((totalDist.index(i) - 384) * (180 / 512)) * i) for i in totalDist] 
-    #verticalDistance = abs(cos((i - 384) * (180 / 512)) * totalDist) 
-    someDistanceAway = 5.5 # Essentially the ground ***
-    lengthOfTheTractor = 3# Horizontal length of the tractor ***
-    obstaclePoints = 0 # Counts how many points break the threshold
-    triggerPoints = 0
-    numberOfPointsNeededToTrigger = 10 # How many points must be seen to trigger a stop? ***
-    sumOfVert = 0
-    sumOfHor = 0
-    i = 15
-    while i < len(totalDist)-15: #Sweep throught the distances
-        if(totalDist[i] < someDistanceAway): # Is there an object that will hit the tractor?
+    someDistanceAway = 4.5 # Essentially the ground ***
+    lengthOfTheTractor = 1.2# Horizontal length of the tractor ***
+    obstaclePoints = 0 # Counts how many points are not the ground
+    triggerPoints = 0 # Counts number of points breaking threshold
+    numberOfPointsNeededToTrigger = 15 # How many points must be seen to trigger a stop? ***
+    sumOfVert = 0 #initializing the sum of the vertical points
+    sumOfHor = 0#initializing the sum of the horizontal points
+    i = 0
+    while i < len(totalDist): #Sweep throught the distances
+        if(totalDist[i] < someDistanceAway): # Is there an object that is not the ground?
             obstaclePoints += 1 # Add a point into the number of obstacle points
             sumOfVert += verticalDistance[i] # kind of calculate average Vertical distance eventually
             sumOfHor += horizontalDistance[i] # kind of calculate average Horizontal distance eventually
-            if(abs(horizontalDistance[i]) < (lengthOfTheTractor / 2.0)):
-                triggerPoints += 1
-             
-            #rospy.loginfo(i)
+            if(abs(horizontalDistance[i]) < (lengthOfTheTractor / 2.0)): #Will the obstacle hit the tractor?
+                triggerPoints += 1 # Add a point the the number of triggers
         i += 1
-    averageVert = Float64()
-    averageHor = Float64()
-    averageNull = Float64()
+    averageVert = Float64() #average vertical distance of the obstacle
+    averageHor = Float64() #average horizontal distance of the obstacle
+    averageNull = Float64() # if there aren't any obstacles
     averageNull.data = -1
-    if(triggerPoints > numberOfPointsNeededToTrigger):
-        #stopTheTractor()    # Whatever code is needed to stop the tractor
-        
-        pub0 = rospy.Publisher('/estop', Bool,  queue_size=10)
+    if(triggerPoints > numberOfPointsNeededToTrigger): # if there is an obstacle that will hit the tractor
+        #  stop the tractor
         pub0.publish(True)
     else:
-        pub0 = rospy.Publisher('/estop', Bool,  queue_size=10)
+        # don't stop the tractor
         pub0.publish(False)
     if(obstaclePoints > 0):
-        averageVert.data = sumOfVert / obstaclePoints
-        averageHor.data = sumOfHor / obstaclePoints    
-        pub1 = rospy.Publisher('/scan_verticals', Float64,  queue_size=10)
-        pub1.publish(averageVert)
-        pub2 = rospy.Publisher('/scan_horizontals', Float64,  queue_size=10)
-        pub2.publish(averageHor) 
-        #rospy.loginfo(obstaclePoints)
+        averageVert.data = sumOfVert / obstaclePoints # Computes average distance of obstacle from tractor
+        averageHor.data = sumOfHor / obstaclePoints # Computes avearge distance from center of tractor
+        pub1.publish(averageVert) #Publishes
+        pub2.publish(averageHor) #Pubslishes
     else:
-        pub1 = rospy.Publisher('/scan_verticals', Float64,  queue_size=10)
-        pub1.publish(averageNull)
-        pub2 = rospy.Publisher('/scan_horizontals', Float64,  queue_size=10)
-        pub2.publish(averageNull)    #rospy.loginfo(totalDist[128])
-def stopTheTractor():
-    rospy.loginfo("Tractor is stopped!")
+        pub1.publish(averageNull) # -1 because no obstacles   
+        pub2.publish(averageNull) # -1 because no obstacles
 
 if __name__ == '__main__':
     listener()
