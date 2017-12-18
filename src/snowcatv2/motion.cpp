9063091b074d545bcda10283d82b8b3ff04c9302
cmoser96 <cm898946@gmail.com>
2017-12-18 05:51:44 -0500

    adding code for snowcat

---
+++ src/snowcatv2/motion.cpp
@@ -0,0 +1,25 @@
+#include "motion.h"
+
+Motion::Motion(ros::NodeHandle *nh, const HardwareSerial *rc, const long baud, const uint8_t address):address(address){
+	status = new ros::Publisher("/roboclawstatus", &stats);
+	cmd = new ros::Subscriber<geometry_msgs::Twist, Motion>("/cmd_vel", &Motion::vel_callback, this);
+	nh->advertise(*status);
+	nh->subscribe(*cmd);
+	roboclaw = new RoboClaw(&rc, 10000);
+	roboclaw.begin(baud);
+	roboclaw.SpeedM1(address, 0);
+	roboclaw.SpeedM2(address, 0);
+}
+
+void Motion::stat_pub(){
+	stats.main_voltage = roboclaw.ReadMainBatteryVoltage(address);
+	stats.logic_voltage = roboclaw.ReadLogicBatteryVoltage(address);
+	roboclaw.ReadCurrents(address, &stats.motor1_current, &stats.motor2_current);
+	stats.error = roboclaw.ReadError(address);
+	status.publish(&stats);
+}
+
+void Motion::vel_callback(const geometry_msgs::Twist &message){
+	roboclaw.DutyAccelM1(address, s, a);
+	roboclaw.DutyAccelM2(address, s, a);
+}
---
+++ src/snowcatv2/snowcatv2.ino
@@ -0,0 +1,88 @@
+/**********************************************************************
+ * Snowcat Code (Teensy 3.5)
+ * @file snowcatv2.ino
+ * @author: Carl Moser
+ * @email: carl.moser@students.olin.edu
+ * @version: 1.0
+ * 
+ * SnowCat code updated to use the OAK architecture
+ * This is the first "full" test of OAK
+ *
+ **********************************************************************/
+
+// Include Libraries
+#include <Arduino.h>
+#include "oak.h"
+#include "motion.h"
+
+// Motion
+#define RC_SERIAL Serial1
+#define RC_BAUD 38400
+#define RC_ADDRESS 0x80
+
+
+// TOF array
+#define NUM_SENSOR 3
+#define LEFT_TOF 1
+#define CENTER_TOF 2
+#define RIGHT_TOF 3
+
+// Plow
+#define PLOW_SERVO_PIN 4
+
+// Lights
+#define LIGHT_PIN 5
+
+// OAK
+OAKServo *plow;
+OAKSoftSwitch *light;
+OAKVL53 *tof_array[NUM_SENSOR];
+const String names[] = {"Left", "Center", "Right"};
+const byte pins[] = {LEFT_TOF, CENTER_TOF, RIGHT_TOF};
+const byte addresses[] = {0x31,0x30,0x29};
+
+// Variables
+ros::NodeHandle nh;
+Motion *motion;
+long last_millis;
+
+
+void setup() {
+  nh.initNode();
+
+  /*******************************************
+  * 
+  * Setting up the TOF array
+  *
+  *******************************************/
+  for(int i = 0; i < NUM_SENSOR; i++){
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], LOW);
+  }
+  delay(15); // Give the TOF sensors time to reset
+  for(int i = 0; i < NUM_SENSOR; i++){
+    digitalWrite(pin[i], HIGH);
+    tof_array[i] = new OAKVL53(&nh, names[i], 100, addresses[i]);
+  }
+
+  /*******************************************
+  * 
+  * Setting up other OAK stuff
+  *
+  *******************************************/
+  plow = new OAKServo(&nh, "/plow", PLOW_SERVO_PIN);
+  light = OAKSoftSwitch(&nh, "/light", LIGHT_PIN);
+  motion = new Motion(&nh, &RC_SERIAL, RC_BAUD, RC_ADDRESS);
+  last_millis = millis();
+}
+
+void loop() {
+  nh.spinOnce();
+  if(millis()-last_millis >= 100){
+    for(int i = 0; i < NUM_SENSOR; i++){
+      tof_array[i]->publish;
+    }
+
+  }
+}
+
