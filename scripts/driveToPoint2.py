d9b3f9adbf8cc416ce90602b412242a1f593607d
Kawin Nikomborirak <concavemail@gmail.com>
2018-04-15 20:44:31 +0000

    update drivetopoint

---
+++ scripts/driveToPoint2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# @author Kawin Nikomborirak
+# @date 2018/04/10
+# @summary node which takes destination coordinates and publishes
+# steering angle and speed
+
+import rospy
+from ackermann_msgs.msg import AckermannDrive
+from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import Float32
+import numpy as np
+from gravl.msg import Hemisphere
+
+
+class driveToPoint:
+    def __init__(self, dest = np.array([42.294055, -71.264568])):
+        rospy.init_node('driveToPoint')
+
+        self.dest = dest
+
+        self.turnrad = 2
+        self.dep = np.array([0, 0])
+        self.altitude = 0
+
+        self.currentHeading = 0.0
+        self.desiredHeading = 0.0
+        self.pubAcker = rospy.Publisher(
+            '/autodrive', AckermannDrive, queue_size=10)
+        self.currentHeadingSub = rospy.Subscriber(
+            '/heading', Hemisphere, self.currentHeadingCallback)
+        self.desiredHeadingSub = rospy.Subscriber(
+            '/course', Float32, self.desiredHeadingCallback)
+        self.currentPositionSub = rospy.Subscriber(
+            '/gps/fix', NavSatFix, self.positionCallback)
+
+        self.ackMsg = AckermannDrive()
+        self.ackMsg.speed = 1
+
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            self.publish()
+            rate.sleep()
+
+    def desiredHeadingCallback(self, data):
+        self.desiredHeading = data.data
+
+    def currentHeadingCallback(self, data):
+        self.currentHeading = data.direction
+
+    def positionCallback(self, data):
+        self.dep = np.array([data.latitude, data.longitude])
+        self.altitude = data.altitude
+
+    def publish(self):
+        radius = 6371000 + self.altitude
+        latToM = np.pi * radius / 180
+        self.ackMsg.steering_angle = np.radians(
+            self.currentHeading - self.desiredHeading)
+        latDist = self.dep - self.dest
+        mDist = latDist * latToM
+        mDist[1] *= np.cos(np.radians(self.dest[0]))
+        d = np.linalg.norm(mDist)
+        print(d)
+
+        self.ackMsg.speed = 1
+        self.pubAcker.publish(self.ackMsg)
+
+
+if __name__ == '__main__':
+    driveToPoint()
--- scripts/gpsToB.py
+++ scripts/gpsToB.py
@@ -16,35 +16,39 @@ class gpsToB:
     longitude in the constructor and publishes the bearing to the
     destination.'''
 
-    def __init__(self, destLat, destLong):
+    def __init__(self, dest):
         rospy.init_node('gpsToB')
         self.course_pub = rospy.Publisher('course', Float32, queue_size=10)
         self.loc_sub = rospy.Subscriber('/gps/fix', NavSatFix, self.callback)
-        self.destLat = destLat
-        self.destLong = destLong
-        try:
-            rospy.spin()
-        except KeyboardInterrupt:
-            print('Shutting down')
+        self.dest = dest
+        self.dep = np.array([0,0])
+        self.depSubscriber = rospy.Subscriber(
+            '/gps/fix', NavSatFix, self.callback)
 
-    def setDestination(self, destLat, destLong):
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            self.publish()
+            rate.sleep()
+
+    def setDestination(self, destLat, destLng):
         '''Set the destination longitude and latitude'''
-        self.destLat = destLat
-        self.destLong = destLong
+        self.dep = np.array([destLat, destLng])
 
     def callback(self, data):
         '''Publish the bearing. pdf here:
         https://badarabbas.files.wordpress.com/2011/03/paper.pdf'''
-        lat1 = np.radians(data.latitude)
-        lon1 = np.radians(data.longitude)
-        lat2 = np.radians(self.destLat)
-        lon2 = np.radians(self.destLong)
-        sina = np.cos(lat2) * np.sin(lon2 - lon1)
-        cosa = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * \
-            np.cos(lat2) * np.cos(lon2 - lon1)
+        self.dep = np.array([data.latitude, data.longitude])
+
+    def publish(self):
+        p1 = np.radians(self.dep)
+        p2 = np.radians(self.dest)
+        sina = np.cos(p2[0]) * np.sin(p2[1] - p1[1])
+        cosa = np.cos(p1[0]) * np.sin(p2[0]) - np.sin(p1[0]) * \
+            np.cos(p2[0]) * np.cos(p2[1] - p1[1])
         angle = np.arctan2(sina, cosa)
         self.course_pub.publish(np.degrees(angle))
 
 
 if __name__ == '__main__':
-    gpsToB(42.294055, -71.264568)
+    gpsToB(np.array([42.294055, -71.264568]))
