078a55c6efd5a47a5625ba072c3196f457dec26a
nathanestill <nathan.estill@students.olin.edu>
2018-09-29 16:58:24 -0400

    modifying LIDarCode

--- scripts/LidarCodeAdvanced.py
+++ scripts/LidarCodeAdvanced.py
@@ -13,20 +13,27 @@ import rospy
 import math
 from std_msgs.msg import Bool
 from sensor_msgs.msg import LaserScan
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Twist, Vector3, Point, Quaternion
+from std_msgs.msg import ColorRGBA, Header
 class ObstacleDetection():
 
     def __init__(self):
         # Variables explained in Github Wiki: https://github.com/olinrobotics/gravl/wiki/Vehicle-Safety
-        rospy.init_node('Obstacle Detection', anonymous=True)
-        self.hasSensed = False # If there is an obstacle sensed
+        rospy.init_node('ObstacleDetection', anonymous=True)
+        self.hasSensedFront = False # If there is an obstacle sensed
+        self.hasSensedDown = False
         self.pubEstop = rospy.Publisher('/softestop', Bool,  queue_size=10) # Publisher that publishes to the estop
         self.frontlaserSub = rospy.Subscriber('/front/scan',LaserScan,self.frontLidarCB) # Subscribes to the front LIDAR, calls the callback function
-        self.downLaserSub = rospy.Subscriber('/down/scan',LaserScan,self.backLidarCB) # Subscribes to the down Lidar, calls the cb function
+        self.downLaserSub = rospy.Subscriber('/down/scan',LaserScan,self.downLidarCB) # Subscribes to the down Lidar, calls the cb function
+        self.frontData = None   
+        self.downData = None
         self.DistanceToTheGround = 4.5 # Essentially the ground
-        self.DownToGround = 1.3
+        self.DownToGround = 1.2 # Distance from down lidar to the ground
         self.widthTractor = 1.25 # Horizontal length of the tractor
-        self.numberOfPointsNeededToTrigger = 15 # How many points must be seen to trigger a stop?
+        self.threshold = 15 # How many points must be seen to trigger a stop?
         self.update_rate = rospy.Rate(5)
+        self.vis_pub = rospy.Publisher('/TractorLine_pt', Marker, queue_size=2)
 
     def frontLidarCB(self,data):
         # Collects data from the Front Hokuyo Lidar and stores it
@@ -36,76 +43,108 @@ class ObstacleDetection():
         # Collects data from the Down Hokuyo Lidar and stores it
         self.downData = data
 
-    def convertToXDistAndYDist(self):
-        self.totalDist = [] # Initializing arrays
-        self.xDist = [] # Distance from front of tractor
-        self.yDist = [] # Distance from center of tractor
+    def convertToXDistAndYDistFront(self):
+        self.totalDistFront = [] # Initializing arrays
+        self.xDistFront = [] # Distance from front of tractor
+        self.yDistFront = [] # Distance from center of tractor
         dataPoints = len(self.frontData.ranges)
         angleSweep = 190.0
         for i in range(len(self.frontData.ranges)): # Puts the tuple of data into x and y Distances
-            self.totalDist.append(self.frontData.ranges[i])      
-            self.xDist.append(abs(math.cos(math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))) * self.totalDist[i])) # Computes the distance from the object
-            self.yDist.append(math.sin(math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))) * self.totalDist[i]) # Computes the distance parallel to tractor
+            self.totalDistFront.append(self.frontData.ranges[i])
+            angleRad = math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))      
+            self.xDistFront.append(abs(math.cos(angleRad) * self.totalDistFront[i])) # Computes the distance from the object
+            self.yDistFront.append(math.sin(angleRad) * self.totalDistFront[i]) # Computes the distance parallel to tractor
 
-    def getNumberOfObstacles(self):
+    def getNumberOfObstaclesFront(self):
         # Calculates the number of points that pose a threat to the tractor
-        self.obstaclePoints = 0 # Counts how many points are not the ground
-        self.triggerPoints = 0 # Counts number of points breaking threshold 
+        self.obstaclePointsFront = 0 # Counts how many points are not the ground
+        self.triggerPointsFront = 0 # Counts number of points breaking threshold 
         for i in range(len(self.totalDist)): # Sweep through the distances
-            if(self.totalDist[i] < self.DistanceToTheGround): # Is there an object that is not the ground?
-                self.obstaclePoints += 1 # Add a point into the number of obstacle points
-                if(abs(self.yDist[i]) < (self.widthTractor / 2.0)): #Will the obstacle hit the tractor?
-                    self.triggerPoints += 1 # Add a point the the number of triggers
+            if(self.totalDistFront[i] < self.DistanceToTheGround): # Is there an object that is not the ground?
+                self.obstaclePointsFront += 1 # Add a point into the number of obstacle points
+                if(abs(self.yDistFront[i]) < (self.widthTractor / 2.0)): #Will the obstacle hit the tractor?
+                    self.triggerPointsFront += 1 # Add a point the the number of triggers
 
-    def sendMessages(self):
+    def sendMessagesFront(self):
         # If the number of trigger points is greater than the threshold, send a singal message to the tractor
-        if(triggerPoints > numberOfPointsNeededToTrigger and not self.hasSensed): # if there is an obstacle that will hit the tractor
+        if(self.triggerPointsFront > self.threshold and not self.hasSensedFront): # if there is an obstacle that will hit the tractor
             # stop the tractor
-            pub0.publish(True)
-            self.hasSensed = True
-        if(triggerPoints <= numberOfPointsNeededToTrigger and self.hasSensed):
+            self.pubEstop.publish(True)
+            self.hasSensedFront = True
+        if(self.triggerPointsFront <= self.threshold and self.hasSensedFront):
             # don't stop the tractor
-            pub0.publish(False)
-            self.hasSensed = False
+            self.pubEstop.publish(False)
+            self.hasSensedFront = False
 
-    def convertToYDistAndZDist(self):
-        self.totalDist = [] # Initializing arrays
-        self.xDist = [] # Distance from front of tractor
-        self.yDist = [] # Distance from center of tractor
-        dataPoints = len(self.frontData.ranges)
-        angleSweep = 270
-        for i in range(len(self.frontData.ranges)): # Puts the tuple of data into x and y Distances
-            self.totalDist.append(self.frontData.ranges[i])      
-            self.zDist.append(abs(math.cos(math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))) * self.totalDist[i])) # Computes the distance from the object
-            self.yDist.append(math.sin(math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))) * self.totalDist[i]) # Computes the distance parallel to tractor
+    def convertToYDistAndZDistDown(self):
+        self.totalDistDown = [] # Initializing arrays
+        self.zDistDown = [] # Distance from front of tractor
+        self.yDistDown = [] # Distance from center of tractor
+        dataPoints = len(self.downData.ranges)
+        angleSweep = 270.0
+        for i in range(len(self.downData.ranges)): # Puts the tuple of data into x and y Distances
+            self.totalDistDown.append(self.downData.ranges[i])
+            angleRad = math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))
+            self.zDistDown.append(abs(math.cos(angleRad) * self.totalDistDown[i])) # Computes the distance from the object
+            self.yDistDown.append(abs(math.sin(angleRad) * self.totalDistDown[i])) # Computes the distance parallel to tractor
 
-    def getNumberOfObstacles(self):
+    def getNumberOfObstaclesDown(self):
         # Calculates the number of points that pose a threat to the tractor
-        self.obstaclePoints = 0 # Counts how many points are not the ground
-        self.triggerPoints = 0 # Counts number of points breaking threshold 
-        for i in range(len(self.totalDist)): # Sweep through the distances
-            if(self.totalDist[i] < self.DownToGround): # Is there an object that is not the ground?
-                self.obstaclePoints += 1 # Add a point into the number of obstacle points
-                if(abs(self.yDist[i]) < (self.widthTractor / 2.0)): #Will the obstacle hit the tractor?
-                    self.triggerPoints += 1 # Add a point the the number of triggers
+        self.obstaclePointsDown = 0 # Counts how many points are not the ground
+        self.triggerPointsDown = 0 # Counts number of points breaking threshold
+        for i in range(len(self.totalDistDown)): # Sweep through the distances
+            if(self.zDistDown[i] < self.DownToGround and self.zDistDown[i] > 0.1): # Is there an object that is not the ground?
+                self.obstaclePointsDown += 1 # Add a point into the number of obstacle points
+                if(abs(self.yDistDown[i]) < (self.widthTractor / 2.0)): #Will the obstacle hit the tractor?
+                    self.triggerPointsDown += 1 # Add a point the the number of triggers
+        used_pts = Marker() # Marker to visualize used lidar pts
+        used_pts.header.frame_id = "laser"
+        used_pts.header.stamp = rospy.Time.now()
+        used_pts.type = 8
+        used_pts.scale = Vector3(100,0.01,0.01)
+        used_pts.color = ColorRGBA(1,1,1,1)
+        used_pts.points = [Point(0,1.25 / 2,0),Point(0,-1.25 / 2,0)]
+        used_pts.pose.orientation.w = 1.0
+        self.vis_pub.publish(used_pts)
 
-    def sendMessages(self):
+
+
+    def sendMessagesDown(self):
         # If the number of trigger points is greater than the threshold, send a singal message to the tractor
-        if(triggerPoints > numberOfPointsNeededToTrigger and not self.hasSensed): # if there is an obstacle that will hit the tractor
+        if(self.triggerPointsDown > self.threshold and not self.hasSensedDown): # if there is an obstacle that will hit the tractor
             # stop the tractor
-            pub0.publish(True)
-            self.hasSensed = True
-        if(triggerPoints <= numberOfPointsNeededToTrigger and self.hasSensed):
+            self.pubEstop.publish(True)
+            self.hasSensedDown = True
+        if(self.triggerPointsDown <= self.threshold and self.hasSensedDown):
             # don't stop the tractor
-            pub0.publish(False)
-            self.hasSensed = False
+            self.pubEstop.publish(False)
+            self.hasSensedDown = False
     def run(self):
         # Runs the code
-        while not rospy.is_shutdown() and (self.frontData == None or self.backData == None):
-            self.convertToXDistAndYDist()
-            self.getNumberOfObstacles()
-            self.sendMessages()
+        while not rospy.is_shutdown() and self.frontData == None and self.downData == None:
+            rospy.logwarn("ERR: Missing data: /front/scan and /down/scan")
+        while not rospy.is_shutdown() and self.frontData == None and self.downData != None:
+            rospy.logwarn("ERR: Missing data: /front/scan (Front Lidar Not Connected)")
+            self.convertToYDistAndZDistDown()
+            self.getNumberOfObstaclesDown()
+            self.sendMessagesDown()
             self.update_rate.sleep()
+        while not rospy.is_shutdown() and self.downData == None and self.frontData != None:
+            rospy.logwarn("ERR: Missing data: /down/scan (Down Lidar Not Connected)")
+            self.convertToXDistAndYDistFront()
+            self.getNumberOfObstaclesFront()
+            self.sendMessagesFront()
+            self.update_rate.sleep()
+        while not rospy.is_shutdown():
+            rospy.loginfo("Running")
+            self.convertToXDistAndYDistFront()
+            self.getNumberOfObstaclesFront()
+            self.sendMessagesFront()
+            self.convertToYDistAndZDistDown()
+            self.getNumberOfObstaclesDown()
+            self.sendMessagesDown()
+            self.update_rate.sleep()
+
 if __name__ == '__main__':
     obs = ObstacleDetection()
     obs.run()
