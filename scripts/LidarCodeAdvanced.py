cdd20b9c53f787b29e7fa89e368c29fe0a2c3882
nathanestill <nathan.estill@students.olin.edu>
2018-10-03 17:55:36 -0400

    made front lidar better, made visualization pretty

--- scripts/LidarCodeAdvanced.py
+++ scripts/LidarCodeAdvanced.py
@@ -28,12 +28,13 @@ class ObstacleDetection():
         self.downLaserSub = rospy.Subscriber('/down/scan',LaserScan,self.downLidarCB) # Subscribes to the down Lidar, calls the cb function
         self.frontData = None   
         self.downData = None
-        self.DistanceToTheGround = 4.5 # Essentially the ground
         self.DownToGround = 1.2 # Distance from down lidar to the ground
         self.widthTractor = 1.25 # Horizontal length of the tractor
         self.threshold = 15 # How many points must be seen to trigger a stop?
         self.update_rate = rospy.Rate(5)
-        self.vis_pub = rospy.Publisher('/TractorLine_pt', Marker, queue_size=2)
+        self.vis_pubFront = rospy.Publisher('/TractorLineFront_pt', Marker, queue_size=2)
+        self.vis_pubDown = rospy.Publisher('/TractorLineDown_pt', Marker, queue_size=2)
+
 
     def frontLidarCB(self,data):
         # Collects data from the Front Hokuyo Lidar and stores it
@@ -47,23 +48,36 @@ class ObstacleDetection():
         self.totalDistFront = [] # Initializing arrays
         self.xDistFront = [] # Distance from front of tractor
         self.yDistFront = [] # Distance from center of tractor
+        self.zDistFront = [] # Distance from the tractor to the point, (usually the ground)
         dataPoints = len(self.frontData.ranges)
         angleSweep = 190.0
+        angleOfIncline = 0.194724 # ?Empirically Discovered. Fix if necessary
         for i in range(len(self.frontData.ranges)): # Puts the tuple of data into x and y Distances
             self.totalDistFront.append(self.frontData.ranges[i])
             angleRad = math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))      
-            self.xDistFront.append(abs(math.cos(angleRad) * self.totalDistFront[i])) # Computes the distance from the object
+            self.xDistFront.append(abs(math.cos(angleOfIncline) * math.cos(angleRad) * self.totalDistFront[i])) # Computes the distance from the object
             self.yDistFront.append(math.sin(angleRad) * self.totalDistFront[i]) # Computes the distance parallel to tractor
+            self.zDistFront.append(math.sin(angleOfIncline) * math.cos(angleRad) * self.totalDistFront[i])
 
-    def getNumberOfObstaclesFront(self):
+    def getNumberOfObstaclesFront(self,visualize):
         # Calculates the number of points that pose a threat to the tractor
         self.obstaclePointsFront = 0 # Counts how many points are not the ground
-        self.triggerPointsFront = 0 # Counts number of points breaking threshold 
-        for i in range(len(self.totalDist)): # Sweep through the distances
-            if(self.totalDistFront[i] < self.DistanceToTheGround): # Is there an object that is not the ground?
+        self.triggerPointsFront = 0 # Counts number of points breaking threshold         
+        for i in range(len(self.totalDistFront)): # Sweep through the distances
+            if(self.zDistFront[i] < self.DownToGround): # Is there an object that is not the ground?
                 self.obstaclePointsFront += 1 # Add a point into the number of obstacle points
                 if(abs(self.yDistFront[i]) < (self.widthTractor / 2.0)): #Will the obstacle hit the tractor?
                     self.triggerPointsFront += 1 # Add a point the the number of triggers
+        if visualize:
+            used_pts = Marker() # Marker to visualize used lidar pts
+            used_pts.header.frame_id = "laser"
+            used_pts.header.stamp = rospy.Time.now()
+            used_pts.type = 8
+            used_pts.scale = Vector3(0.02,0.02,0.02)
+            used_pts.color = ColorRGBA(0,0,1,1)
+            for i in range(len(self.totalDistFront)):
+                used_pts.points.append(Point(self.xDistFront[i],self.yDistFront[i],-self.zDistFront[i]))
+            self.vis_pubFront.publish(used_pts)
 
     def sendMessagesFront(self):
         # If the number of trigger points is greater than the threshold, send a singal message to the tractor
@@ -85,10 +99,10 @@ class ObstacleDetection():
         for i in range(len(self.downData.ranges)): # Puts the tuple of data into x and y Distances
             self.totalDistDown.append(self.downData.ranges[i])
             angleRad = math.radians((i - dataPoints / 2) * (angleSweep / dataPoints))
-            self.zDistDown.append(abs(math.cos(angleRad) * self.totalDistDown[i])) # Computes the distance from the object
-            self.yDistDown.append(abs(math.sin(angleRad) * self.totalDistDown[i])) # Computes the distance parallel to tractor
+            self.zDistDown.append(math.cos(angleRad) * self.totalDistDown[i]) # Computes the distance from the object
+            self.yDistDown.append(math.sin(angleRad) * self.totalDistDown[i]) # Computes the distance parallel to tractor
 
-    def getNumberOfObstaclesDown(self):
+    def getNumberOfObstaclesDown(self,visualize):
         # Calculates the number of points that pose a threat to the tractor
         self.obstaclePointsDown = 0 # Counts how many points are not the ground
         self.triggerPointsDown = 0 # Counts number of points breaking threshold
@@ -97,15 +111,24 @@ class ObstacleDetection():
                 self.obstaclePointsDown += 1 # Add a point into the number of obstacle points
                 if(abs(self.yDistDown[i]) < (self.widthTractor / 2.0)): #Will the obstacle hit the tractor?
                     self.triggerPointsDown += 1 # Add a point the the number of triggers
-        used_pts = Marker() # Marker to visualize used lidar pts
-        used_pts.header.frame_id = "laser"
-        used_pts.header.stamp = rospy.Time.now()
-        used_pts.type = 8
-        used_pts.scale = Vector3(100,0.01,0.01)
-        used_pts.color = ColorRGBA(1,1,1,1)
-        used_pts.points = [Point(0,1.25 / 2,0),Point(0,-1.25 / 2,0)]
-        used_pts.pose.orientation.w = 1.0
-        self.vis_pub.publish(used_pts)
+        if visualize:
+            # used_pts = Marker() # Marker to visualize used lidar pts
+            # used_pts.header.frame_id = "laser"
+            # used_pts.header.stamp = rospy.Time.now()
+            # used_pts.type = 8
+            # used_pts.scale = Vector3(100,0.01,0.01)
+            # used_pts.color = ColorRGBA(1,1,1,1)
+            # used_pts.points = [Point(0,1.25 / 2,0),Point(0,-1.25 / 2,0)]
+            # self.vis_pubDown.publish(used_pts)
+            used_pts = Marker() # Marker to visualize used lidar pts
+            used_pts.header.frame_id = "laser"
+            used_pts.header.stamp = rospy.Time.now()
+            used_pts.type = 8
+            used_pts.scale = Vector3(0.02,0.02,0.02)
+            used_pts.color = ColorRGBA(1,0,0,1)
+            for i in range(len(self.totalDistDown)):
+                used_pts.points.append(Point(0,self.yDistDown[i],-self.zDistDown[i]))
+            self.vis_pubDown.publish(used_pts)
 
 
 
@@ -126,22 +149,22 @@ class ObstacleDetection():
         while not rospy.is_shutdown() and self.frontData == None and self.downData != None:
             rospy.logwarn("ERR: Missing data: /front/scan (Front Lidar Not Connected)")
             self.convertToYDistAndZDistDown()
-            self.getNumberOfObstaclesDown()
+            self.getNumberOfObstaclesDown(True)
             self.sendMessagesDown()
             self.update_rate.sleep()
         while not rospy.is_shutdown() and self.downData == None and self.frontData != None:
             rospy.logwarn("ERR: Missing data: /down/scan (Down Lidar Not Connected)")
             self.convertToXDistAndYDistFront()
-            self.getNumberOfObstaclesFront()
+            self.getNumberOfObstaclesFront(True)
             self.sendMessagesFront()
             self.update_rate.sleep()
+        rospy.loginfo("Running")
         while not rospy.is_shutdown():
-            rospy.loginfo("Running")
             self.convertToXDistAndYDistFront()
-            self.getNumberOfObstaclesFront()
+            self.getNumberOfObstaclesFront(True)
             self.sendMessagesFront()
             self.convertToYDistAndZDistDown()
-            self.getNumberOfObstaclesDown()
+            self.getNumberOfObstaclesDown(True)
             self.sendMessagesDown()
             self.update_rate.sleep()
 
