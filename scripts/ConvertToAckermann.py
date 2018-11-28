7bda7c240430b58dc471a77ba30c21227b1c4d2c
AmyPhung <amyngph@gmail.com>
2018-11-28 17:25:59 -0500

    Restructured to OOP

--- scripts/ConvertToAckermann.py
+++ scripts/ConvertToAckermann.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Subscribes to a twist message and converts it to an ackermann message.
 Subscribes to /twist_vel
@@ -12,107 +13,56 @@ Assumes all inputs are normalized between -1 and 1
 import rospy
 from geometry_msgs.msg import Twist
 from ackermann_msgs.msg import AckermannDrive
-from math import pi
+from scipy.interpolate import interp1d
 
+class ConvertToAckermann():
+    def __init__(self):
+        # Define variables
+        self.twist_data = None
 
-def callback(data, callback_args):
-    """
-    Callback function for subscriber - Converts message + publishes to /cmd_vel
+        # Define ROS constructs
+        rospy.init_node("convert_to_ackermann")
+        self.twist_sub = rospy.Subscriber("/twist_vel", Twist, self.twist_cb)
+        self.ack_pub = rospy.Publisher("/drive", AckermannDrive, queue_size=10)
+        self.update_rate = rospy.Rate(10)
 
-    Args:
-        data - the message picked up by the subscriber - should be a twist message
-        callback_args - list of arguments for function - typically includes
-            publisher object, can be expanded in future to include parameters
-            (ex: max steering velocity, max acceleration, etc.)
-    """
-    # Unpack variables from callback_args - callback args is a list for future support of more parmeters
-    pub = callback_args[0]
+    def twist_cb(self,msg):
+        self.twist_data = msg
 
-    [linear_vel, steering_angle] = convertToAckermann(data.linear.x, data.angular.z)
-    publish(pub, linear_vel, steering_angle)
+    def twist_to_ackermann(self,linear_vel, angular_vel):
+        """
+        Converts linear and angular velocities to linear velocity and steering angle for
+        ackermann messages
 
-def init_subscriber(node, topic, msg_type, callback, callback_args):
-    """
-    Initializes subscriber
+        Args:
+            linear_vel - forward linear velocity from Twist message (should be between -1 and 1)
+            angular_vel - angular velocity from Twist message (should be between -1 and 1)
+        """
+        # Assume twist message is angular vel from -1 to 1, velocity is -1 to 1
+        # steering in degrees from -45 to 45, velocity is -2 to 2
+        vel_scale = interp1d([-1,1],[-2,2])
+        angle_scale = interp1d([-1,1],[-45,45])
 
-    Args:
-        node - name of node
-        topic - name of topic to subscribe to
-        msg_type - message type
-        callback - function to call when new message is recieved
-        callback_args - list of arguments for function - typically includes
-            publisher object, can be expanded in future to include parameters
-            (ex: max steering velocity, max acceleration, etc.)
-    """
-    rospy.init_node(node, anonymous=True) # TODO: What to call this?
-    rospy.Subscriber(topic, msg_type, callback, callback_args)
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+        ack_msg = AckermannDrive()
+        ack_msg.speed = vel_scale(linear_vel)
+        ack_msg.steering_angle = angle_scale(angular_vel)
 
-def init_publisher(node, topic, msg_type):
-    """
-    Initializes publisher
+        return ack_msg
 
-    Args:
-        node - name of node
-        topic - name of topic to publish to
-        msg_type - message type
+    def run(self):
+        # Takes no args, executes timed loop for node
+        while (self.twist_data == None) and not rospy.is_shutdown():
+            print('MSG: No twist data published')
+            self.update_rate.sleep()
 
-    Returns:
-        pub - Publisher object
-    """
-    pub = rospy.Publisher(topic, msg_type, queue_size=10)
-    rospy.init_node(node, anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    return pub
+        while not rospy.is_shutdown():
+            linear = self.twist_data.linear.x
+            angular = self.twist_data.angular.z
 
-def convertToAckermann(linear_vel, angular_vel):
-    """
-    Converts linear and angular velocities to linear velocity and steering angle for
-    ackermann messages
+            ack_msg = self.twist_to_ackermann(linear, angular)
+            self.ack_pub.publish(ack_msg)
+            self.update_rate.sleep()
 
-    Args:
-        linear_vel - forward linear velocity from Twist message (should be between -1 and 1)
-        angular_vel - angular velocity from Twist message (should be between -1 and 1)
-    """
-
-    max_steer_vel = 1 # Sets it to max for now - can be changed later
-    min_steer_vel = 0.1 # Change depending on how fast tractor needs to move while steering
-    max_angle = 1 # Everything is normalized between -1 and 1
-
-    if angular_vel > max_steer_vel:
-        angular_vel = max_steer_vel
-    elif angular_vel < -max_steer_vel:
-        angular_vel = -max_steer_vel
-
-    steering_angle = angular_vel
-
-    if abs(steering_angle) > 0 and linear_vel == 0:
-        linear_vel = min_steer_vel
-    return [linear_vel, steering_angle]
-
-def publish(pub, linear_vel, steering_angle): # AckermannDrive Example: https://answers.ros.org/question/265660/how-to-create-ackermanndrivestamped-messages-in-python/
-    """
-    Publishes ackermann message
-
-    Args:
-        pub - publisher
-        linear_vel - linear velocity in (should be between -1 and 1)
-        steering_angle - steering angle in radians (should be between -1 and 1)
-    """
-    ack_msg = AckermannDrive()
-    ack_msg.speed = linear_vel
-    ack_msg.steering_angle = steering_angle
-    pub.publish(ack_msg)
-
-
-if __name__ == '__main__':
-    try:
-        pub = init_publisher('ConvertToAckermann',"/cmd_vel",AckermannDrive) # TODO: Rename this and node
-
-        callback_args = [pub]
-        # TODO: subscribe to correct twist message source
-        init_subscriber('ConvertToAckermann',"/twist_vel",Twist,callback,callback_args)
-
-    except rospy.ROSInterruptException:
-        pass
+if __name__ == "__main__":
+    convert_to_ackermann = ConvertToAckermann()
+    convert_to_ackermann.run()
