50e2365297a1cb9706d00ce38b49c42f12026c11
Connor <connorrnovak@gmail.com>
2018-11-30 16:33:36 -0500

    Clean formatting, remove reference to spin function

--- src/MainState/MainState.h
+++ src/MainState/MainState.h
@@ -12,28 +12,24 @@
 class MainState{
 public:
   explicit MainState();
-  void spin();
-
 private:
   ros::NodeHandle n;
   ros::Subscriber state_sub;
   ros::Subscriber activate_sub;
   ros::Subscriber behavior_sub;
   ros::Publisher state_pub;
-  std_msgs::UInt8 curr_state;
-  bool is_activated;
   ros::Rate rate;
-  std::vector<Behavior> behavior_vector;
+  std_msgs::UInt8 curr_state;
 
-  // Callback functions
+  std::vector<Behavior> behavior_vector;
+  bool is_activated;
+
   void stateCB(const std_msgs::UInt8& msg);
   void activateCB(const std_msgs::Bool& msg);
   void behaviorCB(const gravl::TwistLabeled& msg);
-
   void setState(std_msgs::UInt8 state);
   void updateBehaviors();
   int getBehavior(int label);
-  //TODO:
 };
 
 #endif //MAIN_STATE_H
