8c783c2c0f871814e03301066303484efb9c9a70
Kawin Nikomborirak <concavemail@gmail.com>
2018-12-16 19:57:34 +0000

    Autopep8

--- scripts/CentersToVector.py
+++ scripts/CentersToVector.py
@@ -2,37 +2,44 @@
 
 import rospy
 import numpy as np
-from sensor_msgs.msg import LaserScan, PointCloud
+from sensor_msgs.msg import PointCloud
 from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Twist, Vector3, Point, Quaternion, Point32, PoseStamped, Pose
-from std_msgs.msg import ColorRGBA, Header, String
+from geometry_msgs.msg import Twist, Vector3, Point, PoseStamped
+from std_msgs.msg import ColorRGBA, Header
 from gravl.msg import TwistLabeled
+
 
 class Gradient():
     def __init__(self):
         rospy.init_node('Gradient', anonymous=True)
-        self.barrelSub = rospy.Subscriber('/barrelCenters',PointCloud,self.barrelCB)
-        self.goalSub = rospy.Subscriber('/move_base_simple/goal',PoseStamped,self.goalPointCB)
-        self.barrelPoints = np.array([[10,10]])
-        self.vectorVisPub = rospy.Publisher('/vector_vis',Marker,queue_size=2)
-        self.tractorTwist = rospy.Publisher('/state_controller/cmd_behavior',TwistLabeled,queue_size=10)
+        self.barrelSub = rospy.Subscriber(
+            '/barrelCenters', PointCloud, self.barrelCB)
+        self.goalSub = rospy.Subscriber(
+            '/move_base_simple/goal', PoseStamped, self.goalPointCB)
+        self.barrelPoints = np.array([[10, 10]])
+        self.vectorVisPub = rospy.Publisher(
+            '/vector_vis', Marker, queue_size=2)
+        self.tractorTwist = rospy.Publisher(
+            '/state_controller/cmd_behavior', TwistLabeled, queue_size=10)
         self.goalPoint = None
 
-    def barrelCB(self,data):
-        self.barrelPoints = np.reshape([], (0,2))
+    def barrelCB(self, data):
+        self.barrelPoints = np.reshape([], (0, 2))
         for i in data.points:
-            self.barrelPoints = np.vstack((self.barrelPoints, [i.x,i.y]))
+            self.barrelPoints = np.vstack((self.barrelPoints, [i.x, i.y]))
         self.barrelPoints = np.array(self.barrelPoints)
+
         print(self.barrelPoints)
 
-    def goalPointCB(self,data):
+    def goalPointCB(self, data):
         self.goalPoint = np.array([data.pose.position.x, data.pose.position.y])
 
     def potential(self):
         '''
         Returns gradient of potential field at origin.
         @param self.barrelPoints a nx2 numpy array with points as rows.
-        @param self.goalPoint a single dimensional numpy array with the self.goalPoint x, y
+        @param self.goalPoint a single dimensional numpy array with
+        the self.goalPoint x, y
         @return 1D numpy array with the gradient at the origin
         '''
 
@@ -54,29 +61,31 @@ class Gradient():
         gradients = np.array(np.gradient(sfc, dx))
         return gradients[:, center, center]
 
-
-    def display(self,vector):
-        vectorMarker = Marker() # marks the path that the tractor will take
+    def display(self, vector):
+        vectorMarker = Marker()  # marks the path that the tractor will take
         vectorMarker.header.frame_id = "laser"
         vectorMarker.header.stamp = rospy.Time.now()
-        vectorMarker.scale = Vector3(0.02,0.10,0.2)
-        vectorMarker.points.append(Point(0,0,0))
-        vectorMarker.points.append(Point(vector[0],vector[1],0))
-        vectorMarker.color = ColorRGBA(0,1,1,1) # white
-        vectorMarker.type = 0 # makes the markers a line that is on every point, (line from 0-1, 1-2, 2-3, 3-4, 4-5, etc.
+        vectorMarker.scale = Vector3(0.02, 0.10, 0.2)
+        vectorMarker.points.append(Point(0, 0, 0))
+        vectorMarker.points.append(Point(vector[0], vector[1], 0))
+        vectorMarker.color = ColorRGBA(0, 1, 1, 1)  # white
+        # makes the markers a line that is on every point, (line from 0-1, 1-2, 2-3, 3-4, 4-5, etc.
+        vectorMarker.type = 0
         self.vectorVisPub.publish(vectorMarker)
 
     def run(self):
         while not rospy.is_shutdown():
-            if(self.goalPoint == None):
-                vector = np.array([0,0])
+            if(self.goalPoint is None):
+                vector = np.array([0, 0])
             else:
                 vector = self.potential()
-            tracTwist = Twist(Vector3(vector[0],0,0),Vector3(0,0,vector[1]))
+            tracTwist = Twist(
+                Vector3(vector[0], 0, 0), Vector3(0, 0, vector[1]))
 
-            tracTwistLabel = TwistLabeled(Header(),"gauntlet",tracTwist)            
+            tracTwistLabel = TwistLabeled(Header(), "gauntlet", tracTwist)
             self.tractorTwist.publish(tracTwistLabel)
             self.display(vector)
+
 
 if __name__ == '__main__':
     grad = Gradient()
